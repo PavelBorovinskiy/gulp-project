2f1abcb3b664303d3809b4579c194c4eaae1f2e7
Pavel Borovinskiy <borovinskiy.job@gmail.com>
2021-12-06 09:08:40 +0300

    Добавление подвала и сжатие всего проекта

--- app/js/main.js
+++ app/js/main.js
@@ -53,4 +53,14 @@ $(document).ready(function() {
         }
     })
 
+
+    $('a[href*="#"]').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        return false;
+    });
+
+
+
 });
